2894cafbd98c2a17365b24e8c8e69ee91b784d1d
Carl <carl@carlossus.com>
2015-06-18 18:06:31 +0800

    config now stores runtime arguments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-var _config = [];
+var minimist = require("minimist");
+var argv = minimist(process.argv.slice(2));
+var _config = argv;
 function config(key, value) {
     if (!value)
         return _config[key];
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import minimist = require("minimist");
-var _config: any = [];
+var argv = minimist(process.argv.slice(2));
+
+var _config: any = argv;
 
 export function config(key: string, value?: string) {
 	if (!value) return _config[key];
