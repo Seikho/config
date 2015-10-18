55229a9979b010646353ad89b96e551762d559e7
Carl <carl@carlossus.com>
2015-10-18 12:59:05 +0800

    Use global object for storage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 var minimist = require("minimist");
 var argv = minimist(process.argv.slice(2));
-var _config = argv;
+global.$longshot = argv;
 function config(key, value) {
     if (!value)
-        return _config[key];
-    _config[key] = value;
+        return global.$longshot[key];
+    global.$longshot[key] = value;
     return value;
 }
 exports.config = config;
